f151f287637bf54c9d1e56a71b8b908ee41ef31d
Ashish <ashish.tutejaa@gmail.com>
2019-04-18 00:35:57 +0530

    All pages completed.  Changes made to Home Page, product desc. page completed, shop page completed.

--- DevBay.js
+++ DevBay.js
@@ -12,18 +12,22 @@ let jumboDisp = document.getElementsByClassName('jumbodisp')[0];
 rightSlid.addEventListener('click',switchR);
 leftSlid.addEventListener('click',switchL);
 let arr = ["https://www.hlimg.com/images/events/738X538/c_1527765363e.jpg","http://www.indiaonlinepages.com/festivals/navratri/gifs/navratri-dates.jpg","https://cdn1.newsstate.com/images/2019/04/03/navratra-21_5.jpg","https://htoindia.com/wp-content/uploads/2017/05/19-amazing-temple-of-south-india.jpg"];
-let counter =0;let jhanda=0;
+let counter =3;let jhanda=0;
 function switchR(e)
 {
+    counter++;
+    if(counter==4)counter=0;
 jumboDisp.style.backgroundImage=`url("${arr[counter]}")`;
-counter++;
-if(counter==4)counter=0;
+
+
 }
 function switchL(e)
 {
-jumboDisp.style.backgroundImage=`url("${arr[counter]}")`;
 counter--;
 if(counter==-1)counter=3;
+jumboDisp.style.backgroundImage=`url("${arr[counter]}")`;
+
+
 }
 
 console.log(socialArr);
@@ -33,3 +37,24 @@ function logIN(e)
 {
 window.location.href='account.html';
 }
+
+var lol = document.getElementsByClassName('sliderRB')[0];
+var collec = lol.getElementsByTagName('div');
+var ays=[];
+var lols = document.getElementsByClassName('sliderPG')[0];
+var collecs = lols.getElementsByTagName('div');
+var ars=[];
+
+for(var i=0;i<collec.length;i+=2)
+{
+var tem = collec[i];
+ays.push(tem);
+}
+ays.forEach(nums=>nums.addEventListener('click',(nums)=>{window.location.href='productdescription.html';}))
+
+for(var j=0;j<collecs.length;j+=2)
+{
+var tems = collecs[j];
+ars.push(tems);
+}
+ars.forEach(num=>num.addEventListener('click',()=>{window.location.href='productdescription.html';}))
