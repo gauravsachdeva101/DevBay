1e01242b61e502ed42414cfe01a648a783f76a09
Ashish <ashish.tutejaa@gmail.com>
2019-06-12 05:04:30 +0530

    Final Version to Push

--- paymentpage.js
+++ paymentpage.js
@@ -8,3 +8,20 @@ function myFunction() {
     text.style.display = "none";
   }
 }
+let button = document.getElementById('tac');
+let para = document.getElementById('tnc');
+let flag = 0;
+button.addEventListener('click',function()
+{
+if(!flag)
+  {
+    para.style.display='block';
+    flag=1;
+
+  }
+else
+  {
+  para.style.display='none';
+  flag=0;
+  }
+});
--- shop.js
+++ shop.js
@@ -1,4 +1,22 @@
-console.log('hi');
+let grid = document.getElementsByClassName('shopnav')[0];
+let list = document.getElementsByClassName('shopnav')[1];
+let sliderRBArr = Array.from(document.getElementsByClassName('sliderRB'));
+let productListView = document.getElementsByClassName('productslistview')[0];
+
+let listViewArr = Array.from(document.getElementsByClassName('itemimage pooja'));
+var sizes = document.getElementsByClassName('product');
+var sizesArr = Array.from(sizes);
+
+grid.addEventListener('click',function(){
+productListView.style.display='none';
+sliderRBArr.forEach(nums=>nums.style.display='flex');
+
+});
+list.addEventListener('click',function(){
+productListView.style.display='block';
+sliderRBArr.forEach(nums=>nums.style.display='none');
+
+});
 let slider = document.getElementById('slider');
 let val = document.getElementById('value');
 let thumb = slider.getElementsByClassName('-webkit-slider-thumb')[0];
@@ -10,7 +28,7 @@ function xor(e)
 val.textContent=`Max Price: Rs.${slider.value}/-`
 }
 
-var sizes = document.getElementsByClassName('product');
-var sizesArr = Array.from(sizes);
 sizesArr.forEach(nums=>nums.addEventListener('click',()=>{window.location.href='productdescription.html'}));
 
+
+listViewArr.forEach(nums=>nums.addEventListener('click',()=>{window.location.href='productdescription.html'}));
